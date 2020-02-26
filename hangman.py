b15ef78c8d9aa146f3ce8bca574483a12404d9d7
Wadkan <wadkan@Wadkan-MacBook-Air.local>
2020-02-26 09:34:16 +0100

    first after change to new repo

---
+++ hangman.py
@@ -0,0 +1,153 @@
+import random
+
+# if used_letters does not exist, and runs on an error, the program declarates it.
+try:
+    test = used_letters
+except NameError:
+    used_letters = []
+
+
+def pick_capital():
+    capitals = ["BUDAPEST", "LONDON", "PARIS", "BERLIN", "ROME", "PRAGUE", "MADRID", "LISBON"]
+    random_capital = random.choice(capitals)
+    return random_capital
+
+
+def get_hashed(word):
+    '''
+    Generates a password based on the word with dashes instead of letters
+    Keeps whitespaces undashed.
+
+    Args:
+    str: The word to hash
+
+    Returns:
+    str: The hashed password
+    '''
+    pass
+
+
+def uncover(hashed_password, password, letter):
+    '''
+    Uncovers all occurences of the given letter in the hashed password based on the password
+
+    Args:
+    str: The hashed password
+    str: The password
+    str: The letter to uncover
+
+    Returns:
+    str: The hashed password with uncovered letter
+    '''
+    pass
+
+
+def update(used_letters, letter):
+    '''
+    Appends the letter to used_letters if it doesn't occur
+
+    Args:
+    list: The list of already used letters
+    str: The letter to append
+
+    Returns:
+    list: The updated list of already used letters
+    '''
+
+
+    for i in password.upper():
+        now_used = letter.upper()
+        if i != letter.upper():
+            pass
+        else:
+            now_used = ""
+            break
+    if now_used != "":
+        used_letters.append(now_used)
+
+# update function test
+# it will update used_letter directly, it has no return value.
+# if the letter is equal to one letter from password, it does not update used_letters list and brake.
+
+"""
+password = "BUDAPEST"
+print(password)
+
+update(used_letters, "r")
+print(used_letters)
+
+update(used_letters, "u")
+print(used_letters)
+
+update(used_letters, "g")
+print(used_letters)
+"""
+
+
+def is_win(hashed_password, password):
+    '''
+    Checks if the hashed password is fully uncovered
+
+    Args:
+    str: The hashed password
+    str: The password
+
+    Returns:
+    bool:
+    '''
+
+    return hashed_password.upper() == password.upper()
+
+# test for is_win function
+""" password = "Budapest"
+print(is_win("B__", "Budapest"))
+"""
+
+def is_loose(life_points):
+    '''
+    Checks if life points is equal 0
+
+    Args:
+    int: The life life_points
+
+    Returns:
+    bool: True if life point is equal 0, False otherwise
+    '''
+    return life_points == 0
+
+"""
+#test for is_loose
+life_points = 3
+for i in range(4):
+    print(i)
+    print(life_points)
+    print(is_loose(life_points - i))
+"""
+
+
+def get_input():
+    '''
+    Reads a user input until it contains only letter
+
+    Returns:
+    str: The validated input
+    '''
+
+    guess = input("Try to guess the word! Only one letter, or a whole word.\n")
+    right_characters = "qwertzuioplkjhgfdsayxcvbnm"
+    for check in right_characters:
+        if check == guess:
+            return True
+    print("Wrong. Use only one or more letter(s) Numbers and special chars are prohibited.")
+    return False
+
+# test for get_input
+# print(get_input())
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
