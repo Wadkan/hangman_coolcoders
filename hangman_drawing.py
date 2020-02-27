9fcccfc0e98ef44ca6b98266b235a12f81f8f6cb
kristof <kristof.erkel@hotmail.com>
2020-02-27 10:23:56 +0100

    Drawing is ready

--- hangman_drawing.py
+++ hangman_drawing.py
@@ -1,3 +1,6 @@
+
+#start from here
+
 def screen(lives):
     if lives == 0:
         print("     ___")
@@ -8,80 +11,81 @@ def screen(lives):
         print("___|___")
     elif lives == 1:
         print("     ___")
-        print("    /   |")
-        print("   |    O")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |    O" + "   USED LETTERS:  " + used_letters)
         print("   |   /|\ ")
-        print("   |   /")
+        print("   |   /" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 2:
         print("     ___")
-        print("    /   |")
-        print("   |    O")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |    O" + "   USED LETTERS:  " + used_letters)
         print("   |   /|\ ")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 3:
         print("     ___")
-        print("    /   |")
-        print("   |    O")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |    O" + "   USED LETTERS:  " + used_letters)
         print("   |   /|")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 4:
         print("     ___")
-        print("    /   |")
-        print("   |    O")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |    O" + "   USED LETTERS:  " + used_letters)
         print("   |    |")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 5:
         print("     ___")
-        print("    /   |")
-        print("   |    O")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |    O" + "   USED LETTERS:  " + used_letters)
         print("   |")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 6:
         print("     ___")
-        print("    /   |")
+        print("    /   |" + "   PASSWORD:      " + hashed_password)
+        print("   |" + "        USED LETTERS:  " + used_letters)
         print("   |")
-        print("   |")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 7:
-        print("     ___")
-        print("    /")
+        print("")
+        print("" + "            PASSWORD:      " + hashed_password)
+        print("   |" + "        USED LETTERS:  " + used_letters)
         print("   |")
-        print("   |")
-        print("   |")
+        print("   |" + " LIVES: " + str(lives))
         print("___|___")
 
     elif lives == 8:
         print("")
+        print("" + "            PASSWORD:      " + hashed_password)
+        print("" + "            USED LETTERS:  " + used_letters)
         print("")
-        print("   |")
-        print("   |")
-        print("   |")
-        print("___|___")
+        print("" + " LIVES: " + str(lives))
+        print("_______")
 
     elif lives == 9:
         print("")
+        print("" + "            PASSWORD:      " + hashed_password)
+        print("" + "            USED LETTERS:  " + used_letters)
         print("")
+        print("" + " LIVES: " + str(lives))
         print("")
-        print("")
-        print("")
-        print("_______")
 
-    elif lives == 10:
-        print("")
-        print("")
-        print("")
-        print("")
-        print("")
-        print("")
+
+hashed_password = "test"
+used_letters = "abcd"
+
+
+
+for i in range(11):
+    print(screen(i))
